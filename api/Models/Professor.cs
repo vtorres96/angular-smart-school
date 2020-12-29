f519158771502cd90602d041cea3e6f419d32b24
vtorres96 <victorcdc96@gmail.com>
2020-12-29 05:09:04 -0300

    added models: Professor

---
+++ api/Models/Professor.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace api.Models
+{
+    public class Professor
+    {
+        public Professor() { }
+        public Professor(int id, string nome, string sobrenome)
+        {
+            this.Id = id;
+            this.Nome = nome;
+            this.Sobrenome = sobrenome;
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public IEnumerable<Disciplina> Disciplinas { get; set; }
+    }
+}
