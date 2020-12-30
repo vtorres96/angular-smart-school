048c2beaa9b20ac742636ca2f7248e7e67cdd257
vtorres96 <victorcdc96@gmail.com>
2020-12-30 02:53:02 -0300

    updated components: added crud professores component

--- web/src/app/professores/professores.component.ts
+++ web/src/app/professores/professores.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Professor } from '../models/professor';
+import { ProfessorService } from "./professor.service";
 
 @Component({
   selector: 'app-professores',
@@ -14,22 +15,32 @@ export class ProfessoresComponent implements OnInit {
   public professorForm: FormGroup;
   public modalRef: BsModalRef;
   public textoSimples: string;
+  public professores: Professor[];
+  public mensagem: object;
   public titulo = 'Professores';
+  public metodo = 'post';
 
-  public professores = [
-    {id: 1, nome: 'Prof Joao 1', sobrenome: 'Sobrenome 1', disciplina: 'Data Science'},
-    {id: 2, nome: 'Prof Joao 2', sobrenome: 'Sobrenome 2', disciplina: 'Desenvolvimento Mobile'},
-    {id: 3, nome: 'Prof Joao 3', sobrenome: 'Sobrenome 3', disciplina: 'UX'},
-    {id: 4, nome: 'Prof Joao 4', sobrenome: 'Sobrenome 4', disciplina: 'Desenvolvimento Web'},
-    {id: 5, nome: 'Prof Joao 5', sobrenome: 'Sobrenome 5', disciplina: 'Data Analytics'},
-    {id: 6, nome: 'Prof Joao 6', sobrenome: 'Sobrenome 6', disciplina: 'Marketing Digital'}
-  ];
-
-  constructor(private fb: FormBuilder, private modalService: BsModalService) {
+  constructor(
+    private fb: FormBuilder, 
+    private modalService: BsModalService,
+    private professorService: ProfessorService
+  ) {
     this.criarForm();
   }
 
   ngOnInit() {
+    this.carregarProfessores();
+  }
+
+  carregarProfessores(){
+    this.professorService.getAll().subscribe(
+      (professores: Professor[]) => { 
+        this.professores = professores 
+      },
+      (erro: any) => {
+        console.error(erro);
+      }
+    );
   }
 
   openModal(template: TemplateRef<any>) {
@@ -38,19 +49,58 @@ export class ProfessoresComponent implements OnInit {
 
   criarForm() {
     this.professorForm = this.fb.group({
+      id: [''],
       nome: ['', Validators.required],
-      sobrenome: ['', Validators.required],
-      disciplina: ['', Validators.required]
+      sobrenome: ['', Validators.required]
     });
   }
 
   professorSubmit() {
-    console.log(this.professorForm.value);
+    this.salvarProfessor(this.professorForm.value);
+  }
+
+  salvarProfessor(professor: Professor){
+    professor.id === 0 ? this.metodo = 'post' : this.metodo = 'put';
+
+    this.professorService[this.metodo](professor).subscribe(
+      () => {
+        this.carregarProfessores();
+      },
+      (erro: any) => {
+        console.log(erro);
+      }
+    );
   }
   
   professorSelect(professor: Professor){
     this.professorSelecionado = professor;
     this.professorForm.patchValue(professor);
+  }
+
+  professorNovo(){
+    this.professorSelecionado = new Professor();
+    this.professorForm.patchValue(this.professorSelecionado);
+  }
+
+  deletar(id: number, ){
+    this.professorService.delete(id).subscribe(
+      (retorno) => {
+        this.mensagem = retorno;
+        this.carregarProfessores();
+      },
+      (erro: any) => {
+        console.log(erro);
+      }
+    );
+
+    this.modalRef.hide();
+
+    setTimeout(function(){
+      let mensagemDeSucesso = document.querySelector('.alert-success')
+      if(mensagemDeSucesso){
+          mensagemDeSucesso.remove()
+      }
+    }, 2000)
   }
 
   voltar(){
