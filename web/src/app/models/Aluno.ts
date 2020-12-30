e09f6d2f77f78c2fcd7b5fc8e35a06bea7c3b7c4
vtorres96 <victorcdc96@gmail.com>
2020-12-30 02:34:08 -0300

    updated models: added constructor in Aluno

--- web/src/app/models/Aluno.ts
+++ web/src/app/models/Aluno.ts
@@ -1,4 +1,11 @@
 export class Aluno {
+  constructor() {
+    this.id = 0;
+    this.nome = '';
+    this.sobrenome = '';
+    this.telefone = '';
+  }
+
   id: number;
   nome: string;
   sobrenome: string;
